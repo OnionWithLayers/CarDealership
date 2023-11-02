2802b9f3e219479449228bd9c4e41791647facf6
DKim-appdev <danhkim2003@gmail.com>
2023-11-02 15:54:47 -0400

    adding getVehiclesByColor method

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,78 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone) {
+        this.inventory = new ArrayList<Vehicle>();
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {
+        ArrayList<Vehicle> vehiclesInPriceRange = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getPrice() >= min && vehicle.getPrice() <= max) {
+                vehiclesInPriceRange.add(vehicle);
+            }
+        }
+        return vehiclesInPriceRange;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        ArrayList<Vehicle> vehicleMakeModel = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getMake().equalsIgnoreCase(make) && vehicle.getModel().equalsIgnoreCase(model)) {
+                vehicleMakeModel.add(vehicle);
+            }
+        }
+        return vehicleMakeModel;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+        ArrayList<Vehicle> vehicleYear = new ArrayList<>();
+        for (Vehicle vehicle : inventory){
+            if (vehicle.getYear() >= min && vehicle.getYear() <= max){
+                vehicleYear.add(vehicle);
+            }
+        }
+        return vehicleYear;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color){
+        ArrayList<Vehicle> vehicleColor = new ArrayList<>();
+        for (int i = 0; i < inventory.size(); i++){
+            
+        }
+    }
 }

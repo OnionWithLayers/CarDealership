5dc726c7007564cba6fbc633a5014b84d3261264
DKim-appdev <danhkim2003@gmail.com>
2023-11-03 11:08:17 -0400

    attempted 'getAllVehicles' method

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Dealership {
     private String name;
@@ -60,59 +61,94 @@ public class Dealership {
         return vehicleMakeModel;
     }
 
-    public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max) {
         ArrayList<Vehicle> vehicleYear = new ArrayList<>();
-        for (Vehicle vehicle : inventory){
-            if (vehicle.getYear() >= min && vehicle.getYear() <= max){
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getYear() >= min && vehicle.getYear() <= max) {
                 vehicleYear.add(vehicle);
             }
         }
         return vehicleYear;
     }
 
-    public ArrayList<Vehicle> getVehiclesByColor(String color){
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
         ArrayList<Vehicle> vehicleColor = new ArrayList<>();
-        for (int i = 0; i < inventory.size(); i++){
-            if (inventory.get(i).getColor().equalsIgnoreCase(color)){
+        for (int i = 0; i < inventory.size(); i++) {
+            if (inventory.get(i).getColor().equalsIgnoreCase(color)) {
                 vehicleColor.add(inventory.get(i));
             }
-        } return vehicleColor;
+        }
+        return vehicleColor;
     }
 
-    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max){
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max) {
         ArrayList<Vehicle> vehicleMileage = new ArrayList<>();
-        for (Vehicle vehicle : inventory){
-            if (vehicle.getOdometer() >= min && vehicle.getOdometer() <= max){
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getOdometer() >= min && vehicle.getOdometer() <= max) {
                 vehicleMileage.add(vehicle);
             }
         }
         return vehicleMileage;
     }
 
-    public ArrayList<Vehicle> getVehiclesByType (String vehicleType){
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {
         ArrayList<Vehicle> vehiclesByType = new ArrayList<>();
-        for (Vehicle vehicle : inventory){
-            if (vehicle.getVehicleType().equalsIgnoreCase(vehicleType)){
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getVehicleType().equalsIgnoreCase(vehicleType)) {
                 vehiclesByType.add(vehicle);
             }
         }
         return vehiclesByType;
     }
 
-    public ArrayList<Vehicle> getAllVehicles(){
+    public ArrayList<Vehicle> getAllVehicles() {
         ArrayList<Vehicle> allVehicle = new ArrayList<>();
         allVehicle.addAll(inventory);
-        return  allVehicle;
+        return allVehicle;
     }
 
     //idk what to do here
-    public ArrayList<Vehicle> addVehicle(){
-        return null;
+    public void addVehicle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's the vin of the vehicle you want to add?");
+        int userVin = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What's the year of the vehicle: ");
+        int userYear = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What's the make of the vehicle: ");
+        String userMake = scanner.nextLine();
+
+        System.out.println("What's the model of the vehicle: ");
+        String userModel = scanner.nextLine();
+
+        System.out.println("Whats the vehicle type: ");
+        String userVehicleType = scanner.nextLine();
+
+        System.out.println("What's the Color of the vehicle: ");
+        String userColor = scanner.nextLine();
+
+        System.out.println("What's the odometer for the vehicle: ");
+        int userOdometer = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What's the price of the vehicle: ");
+        double userPrice = scanner.nextDouble();
+        scanner.nextLine();
+
+        Vehicle userVehicle = new Vehicle(userVin, userYear, userMake, userModel, userVehicleType,
+                userColor, userOdometer, userPrice);
+
+        scanner.close();
+        inventory.add(userVehicle);
     }
 
     //idk what to do here either
-    public ArrayList<Vehicle> remove(){
+    public ArrayList<Vehicle> remove() {
+        System.out.println("What vehicle would you like to remove: ");
 
     }
-    
+
 }
